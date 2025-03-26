122e4e75475bb154a876d0f073d77586bdbbd22b
Khavin Shankar <khavinshankar@gmail.com>
2025-03-26 14:33:34 +0530

    added scan and share qr download feature (#17)

--- src/components/pluggables/FacilityHomeActions.tsx
+++ src/components/pluggables/FacilityHomeActions.tsx
@@ -1,17 +1,17 @@
 import { FC, useState } from "react";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
 import { useTranslation } from "react-i18next";
 import { Button } from "@/components/ui/button";
 import { Facility } from "@/types/facility";
 import { SettingsIcon } from "lucide-react";
 import { ConfigureHealthFacilityForm } from "../ConfigureHealthFacilityForm";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 import { useQueryClient } from "@tanstack/react-query";
 import { I18NNAMESPACE } from "@/lib/constants";
 
@@ -24,7 +24,7 @@ const FacilityHomeActions: FC<FacilityHomeActionsProps> = ({ facility }) => {
   const { t } = useTranslation(I18NNAMESPACE);
   const queryClient = useQueryClient();
 
-  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const [open, setOpen] = useState(false);
 
   if (!facility) {
     return null;
@@ -32,8 +32,8 @@ const FacilityHomeActions: FC<FacilityHomeActionsProps> = ({ facility }) => {
 
   return (
     <>
-      <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
-        <DialogTrigger asChild>
+      <Sheet open={open} onOpenChange={setOpen}>
+        <SheetTrigger asChild>
           <Button
             variant="outline"
             size="sm"
@@ -42,26 +42,27 @@ const FacilityHomeActions: FC<FacilityHomeActionsProps> = ({ facility }) => {
             <SettingsIcon />
             {t("configure_health_facility")}
           </Button>
-        </DialogTrigger>
-        <DialogContent className="max-w-md w-[95%] rounded-md">
-          <DialogHeader>
-            <DialogTitle>{t("configure_health_facility")}</DialogTitle>
-            <DialogDescription className="text-start">
+        </SheetTrigger>
+        <SheetContent className="w-full sm:max-w-2xl overflow-y-auto">
+          <SheetHeader>
+            <SheetTitle>{t("configure_health_facility")}</SheetTitle>
+            <SheetDescription>
               {t("configure_health_facility_description")}
-            </DialogDescription>
-          </DialogHeader>
-
-          <ConfigureHealthFacilityForm
-            facilityId={facility.id}
-            onSuccess={() => {
-              queryClient.invalidateQueries({
-                queryKey: ["healthFacility", facility.id],
-              });
-              setIsDialogOpen(false);
-            }}
-          />
-        </DialogContent>
-      </Dialog>
+            </SheetDescription>
+          </SheetHeader>
+          <div className="mt-6">
+            <ConfigureHealthFacilityForm
+              facilityId={facility.id}
+              onSuccess={() => {
+                queryClient.invalidateQueries({
+                  queryKey: ["healthFacility", facility.id],
+                });
+                setOpen(false);
+              }}
+            />
+          </div>
+        </SheetContent>
+      </Sheet>
     </>
   );
 };
